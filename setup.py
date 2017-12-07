8bc5131560e0b32938953bfc8f497ed3b26d602e
Ken <kenjyco@gmail.com>
2017-12-07 08:42:46 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst', 'r') as fp:
+    long_description = fp.read()
+
+setup(
+    name='fs-helper',
+    version='0.0.1',
+    description='CLI helpers for filesystem tasks',
+    long_description=long_description,
+    author='Ken',
+    author_email='kenjyco@gmail.com',
+    license='MIT',
+    url='https://github.com/kenjyco/fs-helper',
+    download_url='https://github.com/kenjyco/fs-helper/tarball/v0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    package_dir={'': '.'},
+    package_data={
+        '': ['*.ini'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+    ],
+    keywords = ['filesystem', 'files', 'helper']
+)
