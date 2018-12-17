95d47505c25ead89243f65e755f99d6663d2c4f5
Ken <kenjyco@gmail.com>
2018-12-17 10:08:01 -0600

    Bump to v0.0.4

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='fs-helper',
-    version='0.0.3',
+    version='0.0.4',
     description='CLI helpers for filesystem tasks',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/fs-helper',
-    download_url='https://github.com/kenjyco/fs-helper/tarball/v0.0.3',
+    download_url='https://github.com/kenjyco/fs-helper/tarball/v0.0.4',
     packages=find_packages(),
     include_package_data=True,
     package_dir={'': '.'},
